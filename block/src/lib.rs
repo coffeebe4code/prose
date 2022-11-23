e3c5fb45a963bda6eb7b64c02cd3f06c4d77a01f
Chris <chris.clark@coffeeb4code.com>
2022-11-23 13:11:12 -0600

    going to other computer

--- block/src/lib.rs
+++ block/src/lib.rs
@@ -1,4 +1,4 @@
-pub enum Block {
+pub enum BlockKind {
     PlainBlock,
     IfBlock,
     RetBlock,
@@ -6,4 +6,12 @@ pub enum Block {
     FirstBlock,
     TopBlock,
     LeafBlock,
+}
+
+// add instructions vec
+pub struct Block<'a> {
+    kind: BlockKind,
+    preds: Vec<&'a Block<'a>>,
+    succs: Vec<&'a Block<'a>>,
+    id: usize,
 }
