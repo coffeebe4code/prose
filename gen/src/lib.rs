924d3949929c39f32f8005c7a15f2e634a339b7f
Chris <chris.clark@coffeeb4code.com>
2022-11-22 19:03:04 -0600

    pretty good gen for now

--- gen/src/lib.rs
+++ gen/src/lib.rs
@@ -1,5 +1,4 @@
 pub struct Gen {
-    byte_len: usize,
     pos: usize,
     binary: Vec<u8>,
 }
@@ -7,31 +6,44 @@ pub struct Gen {
 impl Gen {
     pub fn new() -> Self {
         Gen {
-            byte_len: 0,
-            binary: vec![0],
+            pos: 0,
+            binary: vec![],
         }
     }
     pub fn add8(&mut self, val: u8) -> &mut Self {
         self.binary.push(val);
-        return self;
-    }
-    pub fn add32_safe(&mut self, first: &[u8; 4], second: &[u8; 4]) -> &mut Self {
-        self.binary.extend_from_slice(first);
-        self.binary.extend_from_slice(second);
+        self.pos += 1;
         return self;
     }
     pub fn add32(&mut self, val: &[u8; 4]) -> &mut Self {
         self.binary.extend_from_slice(val);
+        self.pos += 4;
         return self;
     }
     pub fn add64(&mut self, val: &[u8; 8]) -> &mut Self {
         self.binary.extend_from_slice(val);
+        self.pos += 8;
         return self;
     }
-    pub fn read32(self) -> Option<[u8; 4]> {
-        if self.pos + 4 =< 
-        let ret = self.binary[self.pos];
+    pub fn read32<'a>(&'a mut self) -> &'a [u8] {
+        let ret = &self.binary[self.pos..self.pos + 4];
+        self.pos += 4;
         return ret;
+    }
+    pub fn read64<'a>(&'a mut self) -> &'a [u8] {
+        let ret = &self.binary[self.pos..self.pos + 8];
+        self.pos += 8;
+        return ret;
+    }
+    pub fn get_len(&self) -> usize {
+        return self.binary.len();
+    }
+    pub fn get_remaining(&self) -> usize {
+        return self.binary.len() - self.pos;
+    }
+    pub fn reset(&mut self) -> &mut Self {
+        self.pos = 0;
+        return self;
     }
 }
 
@@ -40,7 +52,21 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        assert_eq!(4, 4);
+    fn it_should_insert_and_read() {
+        let mut gen = Gen::new();
+        gen.add64(&[1; 8]);
+        let val = gen.reset().read64();
+        assert_eq!(val, &[1; 8]);
+    }
+    #[test]
+    fn it_should_insert_with_pos() {
+        let mut gen = Gen::new();
+        gen.add64(&[1; 8]);
+        gen.add32(&[0; 4]);
+        gen.reset();
+        assert_eq!(gen.read64(), &[1; 8]);
+        assert_eq!(gen.get_len(), 12);
+        assert_eq!(gen.get_remaining(), 4);
+        assert_eq!(gen.read32(), &[0; 4]);
     }
 }
