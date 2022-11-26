51c75ed72817313b2b301ac9fba28027b3dbefc9
Chris <chris.clark@coffeeb4code.com>
2022-11-26 08:13:35 -0600

    hash complete for now

--- hash/src/lib.rs
+++ hash/src/lib.rs
@@ -1,9 +1,32 @@
+#[cfg(target_pointer_width = "64")]
+static OFFSET_HASH_BASE: usize = 14695981039346656037;
+#[cfg(target_pointer_width = "32")]
 static OFFSET_HASH_BASE: usize = 2166136261;
 
 pub fn hash(value: &str) -> usize {
     let mut result: usize = 0;
-    for c in value.chars().into_iter() {
-        result = ((c as usize) ^ result) * OFFSET_HASH_BASE;
+    let mut chunks = value.as_bytes().chunks_exact(4);
+    for c in &mut chunks {
+        let bytes = u32::from_ne_bytes(c.try_into().unwrap()) as usize;
+        result = (bytes ^ result).wrapping_mul(OFFSET_HASH_BASE);
     }
+    if chunks.remainder().len() > 0 {
+        for b in chunks.remainder().iter() {
+            result = ((*b as usize) | result).wrapping_mul(OFFSET_HASH_BASE);
+        }
+    }
+
     return result;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_hashes() {
+        let test = "one";
+        let result = hash(test);
+        assert_eq!(1603589979668169483, result);
+    }
 }
