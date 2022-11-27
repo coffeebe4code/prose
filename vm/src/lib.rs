ec30b5ebd7a6376cba9b8352642a27c956bb8600
Chris <chris.clark@coffeeb4code.com>
2022-11-27 11:31:40 -0600

    starting vm and error

--- vm/src/lib.rs
+++ vm/src/lib.rs
@@ -1,14 +1,42 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use gen::GenSource;
+use instr::Instr;
+use opcode::Op;
+
+pub struct Vm {
+    gen: GenSource,
+    regs: [usize; 1024],
 }
 
+impl Vm {
+    pub fn new(gen: GenSource) -> Self {
+        Vm {
+            gen,
+            regs: [0; 1024],
+        }
+    }
+    pub fn run(&mut self) -> Result<usize, usize> {
+        loop {
+            if self.gen.get_remaining() == 0 {
+                break;
+            }
+            let instr = self.gen.read32();
+            let op = Op::from32(instr[0]);
+            match op {
+                _ => {
+                    print!("{}", "developer error");
+                    Err(std::Errorself.regs[0]);
+                }
+            }
+        }
+
+        return Ok(self.regs[0]);
+    }
+}
 #[cfg(test)]
 mod tests {
-    use super::*;
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        assert_eq!(4, 4);
     }
 }
