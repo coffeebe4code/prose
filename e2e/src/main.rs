4d9f0178a64989c15b01089b2b24fc3a5821a420
Chris <chris.clark@coffeeb4code.com>
2023-09-09 14:00:37 -0500

    basic obj files created

--- e2e/src/main.rs
+++ e2e/src/main.rs
@@ -1,10 +1,19 @@
+use ast::*;
 use ir::*;
 use lexer::*;
+use obj::*;
 use parser::*;
 
 fn main() {
-    let lex = ProseLexer::new("pub const x = fn() { return 5 + 3 * 2; }");
+    let lex = ProseLexer::new("pub const main = fn() { return 5; }");
     let mut parse = Parser::new(lex);
-    let ast_parsed = parse.func();
+    let ast_parsed = parse.func().unwrap();
     let mut ir = FIRSource::new(0);
+    match *ast_parsed {
+        Expr::FuncDef(val) => {
+            let result = ir.begin(val);
+            build_main(result);
+        }
+        _ => panic!("not a func def!"),
+    }
 }
