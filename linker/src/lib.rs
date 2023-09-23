8ac5de854f4092b53167e35d94f813152e288ecf
coffeebe4code <chris.clark@coffeeb4code.com>
2023-09-23 16:31:07 -0500

    added an msi installer, and on windows running cl

--- linker/src/lib.rs
+++ linker/src/lib.rs
@@ -2,9 +2,13 @@ use std::path::Path;
 pub fn link(obj_file: &Path, output: &Path) -> Result<(), std::io::Error> {
     use std::io::{Error, ErrorKind};
     use std::process::Command;
+    let mut cmd = "cc";
 
     // link the .o file using host linker
-    let status = Command::new("cc")
+    if cfg!(windows) {
+        cmd = "cl.exe";
+    } 
+    let status = Command::new(cmd)
         .args(&[&obj_file, Path::new("-o"), output])
         .status()
         .map_err(|err| {
